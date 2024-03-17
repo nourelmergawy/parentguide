5969af02912b1882f2eaf12c1f3b745e17c4adbe
nourelmergawy <nouralmergawy@gmail.com>
2024-03-17 19:44:09 +0200

    intial commet and google signin

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin) apply false
+    id("com.google.gms.google-services").version("4.3.14").apply(false) // This line
+
+}
